26518eb26466c7c4c085942780f46eaf528dbab0
Hodaka-A <anxisuigao@gmail.com>
2025-09-02 16:53:15 +0900

    first commit

---
+++ advance-laravel-16/src/resources/views/add.blade.php
@@ -0,0 +1,21 @@
+<form action="/add" method="post">
+  <table>
+  @csrf
+    <tr>
+      <th>name</th>
+      <td><input type="text" name="name"></td>
+    </tr>
+    <tr>
+      <th>age</th>
+      <td><input type="text" name="age"></td>
+    </tr>
+    <tr>
+      <th>nationality</th>
+      <td><input type="text" name="nationality"></td>
+    </tr>
+    <tr>
+      <th></th>
+      <td><button>送信</button></td>
+    </tr>
+  </table>
+</form>
